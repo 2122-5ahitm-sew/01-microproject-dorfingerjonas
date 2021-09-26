777c936f0e2b3af864e8b60604de5df75eb5d8cc
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-09-26 20:54:29 +0200

    dorfinger - implement entity Material.java

---
+++ backend/src/main/java/at/htl/productionfactory/entity/Material.java
@@ -0,0 +1,49 @@
+package at.htl.productionfactory.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Material extends PanacheEntity {
+
+    public String name;
+    public String code;
+    public boolean inStock;
+
+    @OneToMany
+    @Cascade(CascadeType.ALL)
+    public List<Production> productions;
+
+    public Material(Long id, String name, String code, boolean inStock) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.inStock = inStock;
+    }
+
+    public Material(Long id, String name, String code, boolean inStock, List<Production> productions) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.inStock = inStock;
+        this.productions = productions;
+    }
+
+    public Material() {
+    }
+
+    @Override
+    public String toString() {
+        return "Material{" +
+                "name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", inStock=" + inStock +
+                ", productions=" + productions +
+                '}';
+    }
+}
