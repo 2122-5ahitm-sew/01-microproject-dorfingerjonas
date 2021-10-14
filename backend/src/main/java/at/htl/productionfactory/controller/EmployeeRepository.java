ad92d8c17e8bd57b10f755c48b827d5e2197fa30
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:30:23 +0200

    dorfinger - add  ApplicationScoped annotation to EmployeeRepository

--- backend/src/main/java/at/htl/productionfactory/controller/EmployeeRepository.java
+++ backend/src/main/java/at/htl/productionfactory/controller/EmployeeRepository.java
@@ -3,6 +3,9 @@ package at.htl.productionfactory.controller;
 import at.htl.productionfactory.entity.Employee;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class EmployeeRepository implements PanacheRepository<Employee> {
 
 }
