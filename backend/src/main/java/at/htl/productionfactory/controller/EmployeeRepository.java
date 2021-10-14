0425ebc04bc3615d3a835ea397bc775f12d04c75
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:23:00 +0200

    dorfinger - implement EmployeeRepository

---
+++ backend/src/main/java/at/htl/productionfactory/controller/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package at.htl.productionfactory.controller;
+
+import at.htl.productionfactory.entity.Employee;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class EmployeeRepository implements PanacheRepository<Employee> {
+
+}
