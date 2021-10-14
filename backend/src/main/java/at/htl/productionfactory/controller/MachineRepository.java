8ad9c040ff03a1dcdf481b52b540a57013e97549
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:23:13 +0200

    dorfinger - implement MachineRepository

---
+++ backend/src/main/java/at/htl/productionfactory/controller/MachineRepository.java
@@ -0,0 +1,7 @@
+package at.htl.productionfactory.controller;
+
+import at.htl.productionfactory.entity.Machine;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class MachineRepository implements PanacheRepository<Machine> {
+}
