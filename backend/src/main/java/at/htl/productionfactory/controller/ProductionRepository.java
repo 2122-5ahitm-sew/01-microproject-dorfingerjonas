057a2da90fe97ec86b88ddb20f5a93e0b4d9a0b8
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:23:44 +0200

    dorfinger - implement ProductionRepository

---
+++ backend/src/main/java/at/htl/productionfactory/controller/ProductionRepository.java
@@ -0,0 +1,7 @@
+package at.htl.productionfactory.controller;
+
+import at.htl.productionfactory.entity.Production;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class ProductionRepository implements PanacheRepository<Production> {
+}
