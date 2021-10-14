1378bcf531a50dd94350ce8e4f7cd2ff984a495a
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:22:16 +0200

    dorfinger - enhance entity Machine

--- backend/src/main/java/at/htl/productionfactory/entity/Machine.java
+++ backend/src/main/java/at/htl/productionfactory/entity/Machine.java
@@ -1,24 +1,16 @@
 package at.htl.productionfactory.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.OneToMany;
-import java.util.List;
 
 @Entity
 public class Machine extends PanacheEntity {
 
     @Enumerated(EnumType.STRING)
     public MachineType machineType;
-
-    public Machine(MachineType machineType) {
-        this.machineType = machineType;
-    }
 
     public Machine(MachineType machineType) {
         this.machineType = machineType;
