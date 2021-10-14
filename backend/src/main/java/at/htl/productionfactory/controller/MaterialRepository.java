973519f6c18b66e99c26aa1c847b97bef223c874
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 23:35:42 +0200

    dorfinger - implement save() in MaterialRepository

--- backend/src/main/java/at/htl/productionfactory/controller/MaterialRepository.java
+++ backend/src/main/java/at/htl/productionfactory/controller/MaterialRepository.java
@@ -3,5 +3,15 @@ package at.htl.productionfactory.controller;
 import at.htl.productionfactory.entity.Material;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
 public class MaterialRepository implements PanacheRepository<Material> {
+
+    @Transactional
+    public Material save(Material material) {
+        return getEntityManager().merge(material);
+    }
+
 }
