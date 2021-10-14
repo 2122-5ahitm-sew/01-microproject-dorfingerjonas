097cecc0f32f9f39c92c3414c7502a335c9f85d3
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:47:06 +0200

    dorfinger - implement save() in EmployeeRepository

--- backend/src/main/java/at/htl/productionfactory/controller/EmployeeRepository.java
+++ backend/src/main/java/at/htl/productionfactory/controller/EmployeeRepository.java
@@ -4,8 +4,14 @@ import at.htl.productionfactory.entity.Employee;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class EmployeeRepository implements PanacheRepository<Employee> {
+
+    @Transactional
+    public Employee save(Employee employee) {
+        return getEntityManager().merge(employee);
+    }
 
 }
