9bf5e2459f2458d48e583d8a1228ced090f95c69
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-14 22:23:22 +0200

    dorfinger - implement MaterialRepository

---
+++ backend/src/main/java/at/htl/productionfactory/controller/MaterialRepository.java
@@ -0,0 +1,7 @@
+package at.htl.productionfactory.controller;
+
+import at.htl.productionfactory.entity.Material;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class MaterialRepository implements PanacheRepository<Material> {
+}
