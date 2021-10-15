3de573bc61564ce3e19107e6cd87ad9874438c35
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-10-15 06:28:13 +0200

    dorfinger - implement save() in ProductRepository

--- backend/src/main/java/at/htl/productionfactory/controller/ProductRepository.java
+++ backend/src/main/java/at/htl/productionfactory/controller/ProductRepository.java
@@ -3,5 +3,15 @@ package at.htl.productionfactory.controller;
 import at.htl.productionfactory.entity.Product;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
 public class ProductRepository implements PanacheRepository<Product> {
+
+    @Transactional
+    public Product save(Product product) {
+        return getEntityManager().merge(product);
+    }
+
 }
